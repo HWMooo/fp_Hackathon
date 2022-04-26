28505ba3195506afe5fb83c9f976d0ba43192d30
HWMooo <58176899+HWMooo@users.noreply.github.com>
2022-04-26 17:15:25 +0100

    can search by genre

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 from werkzeug import exceptions
+import requests
 app = Flask(__name__, template_folder='./template')
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    variable = request.form['variable']
+    print(variable)
+    data = requests.get('https://www.omdbapi.com/?apikey=6f6461a4&s=' + variable)
+    data = data.json()
+    print(data['Search'])
+    return render_template('films.html', mylist=data['Search'])
 
 
 if __name__ == '__main__':
