3e6917c009073e3ac247c701504c70b8e9a97bc9
HWMooo <58176899+HWMooo@users.noreply.github.com>
2022-04-26 20:49:22 +0100

    has routes for id and on clicking a title will show poster, type and release info

--- app.py
+++ app.py
@@ -4,19 +4,40 @@ from werkzeug import exceptions
 import requests
 app = Flask(__name__, template_folder='./template')
 
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/', methods=['POST'])
-def my_form_post():
-    variable = request.form['variable']
-    print(variable)
+def Get_Films(variable):
     data = requests.get('https://www.omdbapi.com/?apikey=6f6461a4&s=' + variable)
     data = data.json()
-    print(data['Search'])
-    return render_template('films.html', mylist=data['Search'])
+    data = data['Search']
+    return data
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    global variable
+    variable = request.form['variable']
+    data = Get_Films(variable)
+    print(data)
+    return render_template('films.html', mylist=data)
+
+
+
+@app.route('/<string:film_id>', methods=['GET', 'POST'])
+def film_handler(film_id):
+    the_id = ""
+    data = Get_Films(variable)
+    for id in data:
+        if id['imdbID']  == film_id:
+            the_id = id['imdbID']
+            fns = {
+                'GET': id,
+                'POST': variable
+                }
+            return render_template('afilm.html', single_film=id)
+
+
 
 
 if __name__ == '__main__':
