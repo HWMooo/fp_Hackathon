04f63de39e87704bc9ec23e2992bb48dd4cde251
HWMooo <58176899+HWMooo@users.noreply.github.com>
2022-04-26 15:50:36 +0100

    removed anything uneccesary

--- app.py
+++ app.py
@@ -5,10 +5,9 @@ app = Flask(__name__, template_folder='./template')
 
 
 @app.route('/')
-@app.route('/index')
 def index():
     return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
