edfd82a655b393844f07756a324ec2caa26c31f1
HWMooo <58176899+HWMooo@users.noreply.github.com>
2022-04-26 15:49:13 +0100

    finally added index.html and connected to server

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 from werkzeug import exceptions
-app = Flask(__name__)
-CORS(app)
+app = Flask(__name__, template_folder='./template')
 
 
 @app.route('/')
-def welcome():
-    return 'Welcome to Flask!!!'
-
+@app.route('/index')
+def index():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
